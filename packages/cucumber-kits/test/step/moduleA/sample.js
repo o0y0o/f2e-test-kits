be7600b5580987846cc66a7ee1b401611d19e8dc
Jason Chung <jason.chung@skylinetw.com>
2021-04-09 18:53:59 +0800

    fix: linter warning

--- packages/cucumber-kits/test/step/moduleA/sample.js
+++ packages/cucumber-kits/test/step/moduleA/sample.js
@@ -1,5 +1,5 @@
 const { defineStep } = require('cucumber')
 
-defineStep(/^(.+) has done something$/, function (who) {})
-defineStep(/^(.+) does some actions$/, function (who) {})
-defineStep(/^(.+) will get expected results$/, function (who) {})
+defineStep(/^(.+) has done something$/, function () {})
+defineStep(/^(.+) does some actions$/, function () {})
+defineStep(/^(.+) will get expected results$/, function () {})
